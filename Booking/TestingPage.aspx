96a8bc5a525e4e8e5d33c11dbe14965fabb353c5
Mugbearer <hobeypadlanxx@gmail.com>
2023-02-11 18:09:18 +0800

    connected database to project
    I added a new page just for testing. In that page, I connected the database.

---
+++ Booking/TestingPage.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.Master" AutoEventWireup="true" CodeBehind="TestingPage.aspx.cs" Inherits="Booking.TestingPage" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" style="height: 26px" Text="Button" />
+    <br />
+    <asp:Label ID="output" runat="server" Text="Label"></asp:Label>
+</asp:Content>
---
+++ Booking/TestingPage.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using MySqlConnector;
+
+namespace Booking
+{
+    public partial class TestingPage : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //OleDbConnection bookingConnect = new OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" + Server.MapPath("jdbc:mysql://sql12.freesqldatabase.com:3306/sql12597390"));
+            string connectionString = "server=sql12.freesqldatabase.com;user=sql12597390;database=sql12597390;password=4GJ2JL9HDl;";
+            MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
+            mySqlConnection.Open();
+            string outputStr = $"SELECT * FROM bookingTBL;";
+            MySqlCommand mySqlCommand = new MySqlCommand(outputStr,mySqlConnection);
+            //mySqlCommand.ExecuteReader();
+            var reader = mySqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                output.Text = reader[1].ToString();
+            }
+            //output.Text = ;
+        }
+    }
+}
