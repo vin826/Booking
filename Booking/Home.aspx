c6493d9eebcce296d9d45e5c9077b9cfcab5b275
Mugbearer <hobeypadlanxx@gmail.com>
2023-02-21 15:02:26 +0800

    s

--- Booking/Home.aspx
+++ Booking/Home.aspx
@@ -2,5 +2,5 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
-    <asp:Image ID="imgOne" runat="server" />
+    <asp:Image ID="imgOne" runat="server" ImageUrl="~/Images/imgOne.jpg" />
 </asp:Content>
--- Booking/MasterPage.Master.designer.cs
+++ Booking/MasterPage.Master.designer.cs
@@ -33,15 +33,6 @@ namespace Booking
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
 
         /// <summary>
-        /// LogButton control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.LinkButton LogButton;
-
-        /// <summary>
         /// HyperLink1 control.
         /// </summary>
         /// <remarks>
