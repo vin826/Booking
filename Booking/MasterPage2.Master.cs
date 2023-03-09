9c4e0cd529d4448219e2964af878d0361def057c
Mugbearer <hobeypadlanxx@gmail.com>
2023-03-09 22:15:22 +0800

    css stuff, deleted unnecessary files

--- Booking/MasterPage2.Master.cs
+++ Booking/MasterPage2.Master.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace Booking
 {
-    public partial class WebForm1 : System.Web.UI.Page
+    public partial class MasterPage2 : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
